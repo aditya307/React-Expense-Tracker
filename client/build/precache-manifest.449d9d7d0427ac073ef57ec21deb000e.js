508590e304505f03fe7ad20e80e2d539a574c9b1
AA <adi.2002.gwl@gmail.com>
2020-06-01 23:18:24 +0530

    Added build files

---
+++ client/build/precache-manifest.449d9d7d0427ac073ef57ec21deb000e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd3f7991529150891670393a1b1cd71a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "086631de929a9525c093",
+    "url": "/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "98d20d4aafcf0b5978ca",
+    "url": "/static/js/2.b5f5f0fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b5f5f0fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "086631de929a9525c093",
+    "url": "/static/js/main.403e8535.chunk.js"
+  },
+  {
+    "revision": "0fb5aa10d11e0a3e8d13",
+    "url": "/static/js/runtime-main.56c06abf.js"
+  }
+]);
