fca02de410262b443dd465ab399d74873777bb34
AA <adi.2002.gwl@gmail.com>
2020-06-06 23:28:49 +0530

    upadated login data

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -1,82 +1,82 @@
-const Transaction = require('../models/Transaction');
+const Transaction = require("../models/Transaction");
 
 // @desc get all trans
 // @route GET /api/v1/transactions
 // @access Public
 
-exports.getTransactions =async (req, res, next) => {
-    try {
-        const transactions = await Transaction.find();
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find({ user: req.user._id });
 
-        return res.status(200).json({
-            success: true,
-            count:transactions.length,
-            data: transactions
-        });
-    } catch (err) {
-        return res.status(500).json({
-            success: false,
-            error: 'Server Error'  
-        });
-    }
-}
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: "Server Error",
+    });
+  }
+};
 
 // @desc add trans
 // @route POSt /api/v1/transactions
 // @access Public
 
-exports.addTransactions =async (req, res,next) => {
-    try {
-        const { text, amount} = req.body;
+exports.addTransactions = async (req, res, next) => {
+  try {
+    const { text, amount } = req.body;
 
-        const transaction = await Transaction.create(req.body);
+    const transaction = await Transaction.create(req.body);
 
-        return res.status(201).json({
-            success: true,
-            data: transaction
+    return res.status(201).json({
+      success: true,
+      data: transaction,
     });
-    } catch (err){
-        if(err.name === 'ValidationError'){
-            const messages = Object.values(err.errors).map(val => val.message);
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      const messages = Object.values(err.errors).map((val) => val.message);
 
-            return res.status(400).json({
-                success: false,
-                error: messages
-            });
-        } else {
-            return res.status(500).json({
-                success: false,
-                error: 'Server Error'  
-            });
-        }
+      return res.status(400).json({
+        success: false,
+        error: messages,
+      });
+    } else {
+      return res.status(500).json({
+        success: false,
+        error: "Server Error",
+      });
     }
-}
+  }
+};
 
 // @desc Delete trans
 // @route Delte /api/v1/transactions
 // @access Public
 
-exports.deleteTransactions = async(req, res, next) => {
-    try {
-        const transaction = await Transaction.findById(req.params.id);
+exports.deleteTransactions = async (req, res, next) => {
+  try {
+    const transaction = await Transaction.findById(req.params.id);
 
-        if(!transaction) {
-            return res.status(404).json({
-                success: false,
-                error: 'Nope'
-            });
-        }
-        
-        await transaction.remove();
-
-        return res.status(200).json({
-            success: true,
-            data: {}
-        });
-    } catch(err) {
-        return res.status(500).json({
-            success: false,
-            error: 'Server Error'  
-        });
+    if (!transaction) {
+      return res.status(404).json({
+        success: false,
+        error: "Nope",
+      });
     }
-}
+
+    await transaction.remove();
+
+    return res.status(200).json({
+      success: true,
+      data: {},
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: "Server Error",
+    });
+  }
+};
